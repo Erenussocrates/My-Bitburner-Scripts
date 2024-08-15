5dd0a2c360f87490f4fd2e1d0610f15b2718efd3
Erenus Özer <erenus_eflatun@hotmail.com>
2024-08-15 15:12:08 +0300

    Update fastStart.js

--- fastStart.js
+++ fastStart.js
@@ -1,12 +1,10 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
-ns.scriptKill('payday.js', "home");
 ns.scriptKill('distFarm.js', "home");
 ns.scriptKill('macroFarmStart.js', "home");
 ns.scriptKill('masterFarm.js', "home");
 
-ns.exec('payday.js', "home", 5)
 ns.exec('distFarm.js', "home")
 ns.exec('macroFarmStart.js', "home")
 ns.exec('masterFarm.js', "home")
