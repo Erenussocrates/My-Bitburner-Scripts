28b6e48d562dd5c9d241ad52292c8eb4c0c5753a
Erenus Özer <erenus_eflatun@hotmail.com>
2024-08-15 15:12:46 +0300

    Update macroAnalyze.js

--- macroAnalyze.js
+++ macroAnalyze.js
@@ -7,25 +7,29 @@ export async function main(ns) {
     for (let server of servers) {
         // Gather server information
         let minSecurity = ns.getServerMinSecurityLevel(server);
-        let maxMoney = ns.getServerMaxMoney(server);
+        let maxMoney = Number(ns.getServerMaxMoney(server));
         let currentSecurity = ns.getServerSecurityLevel(server);
-        let currentMoney = ns.getServerMoneyAvailable(server);
+        let currentMoney = Number(ns.getServerMoneyAvailable(server));
         let maxRam = ns.getServerMaxRam(server);
         let usedRam = ns.getServerUsedRam(server);
         let hasRoot = ns.hasRootAccess(server);
+        let growthRate = ns.getServerGrowth(server);
 
         // Print the server information
         ns.tprint(`Server: ${server}`);
         ns.tprint(`Root Access: ${hasRoot ? 'Yes' : 'No'}`);
-        ns.tprint(`Current Money: ${ns.nFormat(currentMoney, '$0.000a')}`);
-        ns.tprint(`Max Money: ${ns.nFormat(maxMoney, '$0.000a')}`);
+        //ns.tprint(`Current Money: ${ns.nFormat(currentMoney, '$0.000a')}`);
+        ns.tprint(`Current Money: ${ns.formatNumber(currentMoney, 3)}`);
+        //ns.tprint(`Max Money: ${ns.nFormat(maxMoney, '$0.000a')}`);
+        ns.tprint(`Max Money: ${ns.formatNumber(maxMoney, 3)}`);
         ns.tprint(`Current Security Level: ${currentSecurity}`);
         ns.tprint(`Min Security Level: ${minSecurity}`);
         ns.tprint(`RAM Usage: ${usedRam.toFixed(2)} / ${maxRam.toFixed(2)} GB`);
         ns.tprint(`Current Money Percentage: ${(100 * currentMoney / maxMoney).toFixed(2)} %`);
+        ns.tprint(`Growth Rate: ${growthRate}`);
         ns.tprint('-----------------------------------');
 
         // Adding a delay to avoid spamming
-        await ns.sleep(2000); // 2 seconds
+        await ns.sleep(100);
     }
 }
