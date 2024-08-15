38d73e7a7609a2e4428689a3b42c0eb97ef2f4f0
Erenus Özer <erenus_eflatun@hotmail.com>
2024-08-15 15:11:24 +0300

    Update distFarm.js

--- distFarm.js
+++ distFarm.js
@@ -2,16 +2,20 @@
 export async function main(ns) {
 
     async function copyfiles(server) {
-        ns.scp('masterFarm.js', server, 'home');
-        ns.scp('masterGrow.js', server, 'home');
-        ns.scp('masterWeaken.js', server, 'home');
+        ns.scp('all-list.txt', server, 'home');
+        ns.scp('stock-list.txt', server, 'home');
+        ns.scp('myOwnServers.txt', server, 'home');
+        ns.scp('clientFarm.js', server, 'home');
+        ns.scp('masterHack.js', server, 'home');
+    }
+    async function lowball(server){
+        ns.scp('all-list.txt', server, 'home');
+        ns.scp('stock-list.txt', server, 'home');
+        ns.scp('lowballHack.js', server, 'home');
     }
 
-    async function lowball(server) {
-      ns.scp('payday.js', server, 'home');
-    }
-
-    let data = ns.read('distribution-list.txt');
+    //let data = ns.read('distribution-list.txt');
+    let data = ns.read('all-list.txt');
     let servers = data.split('\n').map(s => s.trim()).filter(s => s !== '');
 
     for (let server of servers) {
@@ -20,24 +24,25 @@ export async function main(ns) {
                 ns.print(`No root access to ${server}, skipping...`);
                 continue; // Skip this server if no root access
             }
-        
-        ns.scp('combined-list.txt', server, 'home');
-
-        let maxRam = ns.getServerMaxRam(server);
-        if (maxRam <= 8) {
+        if (ns.getServerMaxRam(server)<=4) {
             await lowball(server);
-        } else {
-            await copyfiles(server);
+        }
+        else if(ns.getServerMaxRam(server)>4){
+          //ns.scp('combined-list.txt', server, 'home');
+          await copyfiles(server);
+        }
+        else if (ns.getServerMaxRam(server)<=0){
+          ns.print(`${server} has no RAM, skipping...`);
+          continue; // Skip this server if no root access
         }
     }
+
 
     let data2 = ns.read('myOwnServers.txt');
     let myServers = data2.split('\n').map(s => s.trim()).filter(s => s !== '');
 
     for (let server of myServers) {
-        ns.scp('combined-list.txt', server, 'home');
-        let maxRam = ns.getServerMaxRam(server);
-        if (maxRam <= 8) {
+        if (ns.getServerMaxRam(server) <= 4) {
             await lowball(server);
         } else {
             await copyfiles(server);
